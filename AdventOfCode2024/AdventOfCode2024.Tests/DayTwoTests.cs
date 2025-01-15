57292cd39e37c1c6bedb320b93934d411c0790a4
Sascha Klomp <sascha.klomp@inspark.nl>
2025-01-15 13:56:18 +0100

    Day 2 start

---
+++ AdventOfCode2024/AdventOfCode2024.Tests/DayTwoTests.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode2024.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Tests
+{
+    public class DayTwoTests
+    {
+
+        [Theory]
+        [InlineData("SampleDayTwo-1", 2)]
+        public void DayTwoStarOne_Should_Correctly_Calculate(string filepath, int expected)
+        {
+            // Arrange
+            var starOne = new TwoStarOne();
+
+            // Act
+            var result = starOne.Run(filepath);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ AdventOfCode2024/AdventOfCode2024/Challenges/TwoStarOne.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2024.Challenges
+{
+    public class TwoStarOne : IChallenge
+    {
+        public int Run(string filePath)
+        {
+            var input = FileLoaderService.LoadFile(filePath)
+                                .Split("\n")
+                                .Select(x => x.TrimEnd().Split(" ").Select(y => int.Parse(y)).ToArray())
+                                .ToArray();
+
+            var foo = input.Select((x) => x.Select((t, i) => (value: t, index: i)).Any(z => z.value - (z.index > x.Length-1 ? 0 : x[z.index + 1]) > 3 || z.value - (z.index > x.Length - 1 ? 0 : x[z.index + 1]) < -1));
+
+            return 0;        
+        }
+    }
+}
